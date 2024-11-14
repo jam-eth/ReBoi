e608c99935710c16daa4fdaed582df728046e2de
jam-eth <65913319+jam-eth@users.noreply.github.com>
2024-11-14 11:59:24 +0000

    Update usb_keypad.py

--- usb_keypad.py
+++ usb_keypad.py
@@ -3,19 +3,14 @@ import digitalio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
-from adafruit_hid.consumer_control import ConsumerControl
-from adafruit_hid.consumer_control_code import ConsumerControlCode
 import time
 
 # Initialize the keyboard and consumer control
 kbd = Keyboard(usb_hid.devices)
-consumer_ctrl = ConsumerControl(usb_hid.devices)
 
 # Setup buttons (replace pins with actual GPIO)
 button_A = digitalio.DigitalInOut(board.GP0)
 button_B = digitalio.DigitalInOut(board.GP1)
-button_rUp = digitalio.DigitalInOut(board.GP21)
-button_rDown = digitalio.DigitalInOut(board.GP23)
 button_Up = digitalio.DigitalInOut(board.GP12)
 button_Down = digitalio.DigitalInOut(board.GP15)
 button_Left = digitalio.DigitalInOut(board.GP14)
@@ -24,26 +19,24 @@ button_Start = digitalio.DigitalInOut(board.GP25)
 button_Select = digitalio.DigitalInOut(board.GP24)
 
 # Configure as inputs with pull-ups
-for btn in [button_A, button_B, button_rUp, button_rDown, button_Up, button_Down, button_Left, button_Right, button_Start, button_Select]:
+for btn in [button_A, button_B, button_Up, button_Down, button_Left, button_Right, button_Start, button_Select]:
     btn.direction = digitalio.Direction.INPUT
     btn.pull = digitalio.Pull.UP
 
 button_states = {
     "A": {"pressed": False, "last_press_time": 0},
     "B": {"pressed": False, "last_press_time": 0},
-    "rUp": {"pressed": False, "last_press_time": 0},
     "Up": {"pressed": False, "last_press_time": 0},
     "Down": {"pressed": False, "last_press_time": 0},
     "Left": {"pressed": False, "last_press_time": 0},
     "Right": {"pressed": False, "last_press_time": 0},
     "Start": {"pressed": False, "last_press_time": 0},
     "Select": {"pressed": False, "last_press_time": 0},
-    "rDown": {"pressed": False, "last_press_time": 0}
 }
 
 DEBOUNCE_DELAY = 0.01
 
-def send_key_on_press(button, keycode, state_name, is_consumer=False):
+def send_key_on_press(button, keycode, state_name):
     current_time = time.monotonic()
     if not button.value and not button_states[state_name]["pressed"]:
         if (current_time - button_states[state_name]["last_press_time"]) > DEBOUNCE_DELAY:
@@ -70,5 +63,3 @@ def check_buttons():
     send_key_on_press(button_Right, Keycode.RIGHT_ARROW, "Right")
     send_key_on_press(button_Start, Keycode.ENTER, "Start")
     send_key_on_press(button_Select, Keycode.TAB, "Select")
-    send_key_on_press(button_rUp, ConsumerControlCode.VOLUME_INCREMENT, "rUp", is_consumer=True)
-    send_key_on_press(button_rDown, ConsumerControlCode.VOLUME_DECREMENT, "rDown", is_consumer=True)
